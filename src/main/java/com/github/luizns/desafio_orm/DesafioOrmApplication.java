1a2448e2440aef10d50c02e3b458af3743add590
Luiz Nascimento <luns.wm@hotmail.com>
2024-10-14 14:36:27 -0300

    initial project

---
+++ src/main/java/com/github/luizns/desafio_orm/DesafioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.github.luizns.desafio_orm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioOrmApplication.class, args);
+	}
+
+}
